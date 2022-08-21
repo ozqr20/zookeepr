6f4c5d57894ef2c72397df5441c8bfb2469e60f7
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-08-20 22:58:25 -0400

    working on seting up the functionality in the back end

--- server.js
+++ server.js
@@ -5,10 +5,18 @@ const path = require('path');
 // instantiate the server
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+///  Middleware helper functions
+
+// It makes certain files readily avaliable
+app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({esteended: true}));
 // parse incoming JSON data
 app.use(express.json());
+
+/// 
+
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -96,6 +104,11 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+// / points to the root route of the server
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
@@ -108,6 +121,8 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
